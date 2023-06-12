6a950f91b814aecb69517ae97352f9ee904532eb
Uma <umcgorai@in.ibm.com>
2023-06-12 16:00:07 +0530

    Added new code

---
+++ aem-micron-web/core/src/main/java/com/micron/web/core/solr/SolrCAConfig.java
@@ -0,0 +1,18 @@
+package com.micron.web.core.solr;
+
+import org.apache.sling.caconfig.annotation.Configuration;
+import org.apache.sling.caconfig.annotation.Property;
+
+@Configuration(label = "Solr CA Config", collection = true, description = "Context aware configuration for solr")
+public @interface SolrCAConfig {
+
+    @Property(label = "Site Id", description = "Site Id")
+    String siteId();
+
+    @Property(label = "Core name", description = "Core name for each site")
+    String collectionName();
+
+    @Property(label = "Filed names", description = "Filed names")
+    String filedNames();
+
+}
---
+++ aem-micron-web/core/src/main/java/com/micron/web/core/solr/SolrOsgiConfig.java
@@ -0,0 +1,25 @@
+package com.micron.web.core.solr;
+
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+
+@ObjectClassDefinition(name = "Solr osgi configuration", description = "Osgi configuration for solr")
+public @interface SolrOsgiConfig {
+
+    @AttributeDefinition(name = "Site Id")
+    String siteId();
+
+    @AttributeDefinition(name = "Server end point")
+    String solrServerEndPoint();
+
+    @AttributeDefinition(name = "User name")
+    String solrUserName();
+
+    @AttributeDefinition(name = "Password ")
+    String solrPassword();
+
+    @AttributeDefinition(name = "Solr response fields")
+    String[] solrResponseFiled();
+
+
+}
